bc8f0276b62cd1b0e852593e71d1c12c6cb1f058
n7-remax <ditritusa@gmail.com>
2021-01-22 16:21:29 +0200

    created login component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,14 @@
 import "../style/style.css";
+import Login from "./Login/Login";
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <div>
+        <Login />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div className="login">login</div>;
+};
+
+export default Login;
